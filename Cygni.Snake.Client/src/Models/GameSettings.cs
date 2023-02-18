00d115e4dd75d8efd81a74e272745692e255eace
Mårten Åsberg <marten.asberg@outlook.com>
2023-02-18 17:02:58 +0100

    GameSettings from Java project

--- Cygni.Snake.Client/src/Models/GameSettings.cs
+++ Cygni.Snake.Client/src/Models/GameSettings.cs
@@ -4,12 +4,22 @@ namespace Cygni.Snake.Client.Models;
 
 public record GameSettings(
 	[property: JsonPropertyName("maxNoofPlayers")]
-	int MaxNoOfPlayers = 5,
+	int MaxNumberOfPlayers = 5,
 	int StartSnakeLength = 1,
 	int TimeInMsPerTick = 250,
-	bool ObstaclesEnabled = false,
+	bool ObstaclesEnabled = true,
 	bool FoodEnabled = true,
-	bool HeadToTailConsumes = false,
+	bool HeadToTailConsumes = true,
 	bool TailConsumeGrows = false,
 	int AddFoodLikelihood = 15,
-	int RemoveFoodLikelihood = 5);
+	int RemoveFoodLikelihood = 5,
+	int SpontaneousGrowthEveryNWorldTick = 3,
+	bool TrainingGame = true,
+	int PointsPerLength = 1,
+	int PointsPerFood = 2,
+	int PointsPerCausedDeath = 5,
+	int PointsPerNibble = 10,
+	[property: JsonPropertyName("noofRoundsTailProtectedAfterNibble")]
+	int NumberOfRoundsTailProtectedAfterNibble = 3,
+	int StartFood = 0,
+	int StartObstacles = 5);
