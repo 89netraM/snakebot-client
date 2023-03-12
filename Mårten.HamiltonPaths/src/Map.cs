031780c84eb6eb769599cc3fb96a082a8c2dd4f4
Mårten Åsberg <marten.asberg@outlook.com>
2023-03-12 17:37:34 +0100

    Hamilton Path: Hamilton Cycle

--- Mårten.HamiltonPaths/src/Map.cs
+++ Mårten.HamiltonPaths/src/Map.cs
@@ -1,8 +1,6 @@
 using System;
 using System.Collections.Generic;
-using System.Collections.Immutable;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace Mårten.HamiltonPaths;
 
@@ -20,35 +18,89 @@ public class Map
 		Path = Enumerable.Empty<Vector2>();
 		if (Start is not null && End is not null)
 		{
-			Task.Run(RouteCalculation);
+			RouteCalculation();
 		}
 	}
 
 	private void RouteCalculation()
 	{
-		var pathTo = new Dictionary<Vector2, IImmutableList<Vector2>>();
-		pathTo.Add(Start!, ImmutableList.Create<Vector2>(Start!));
-		var toVisit = new Queue<Vector2>();
-		toVisit.Enqueue(Start!);
-
-		while (toVisit.TryDequeue(out var current))
+		var zero = new Vector2(0, 0);
+		var pathFrom = HamiltonCycle(zero, new(Width, Height));
+		var path = new List<Vector2> { zero, pathFrom[zero] };
+		while (path[^1] != zero)
 		{
-			var path = pathTo[current];
-			if (current == End)
+			path.Add(pathFrom[path[^1]]);
+		}
+		Path = path;
+	}
+
+	private IDictionary<Vector2, Vector2> HamiltonCycle(Vector2 offset, Vector2 size)
+	{
+		if (size.X == 1 && size.Y == 1)
+		{
+			return new Dictionary<Vector2, Vector2> { [offset] = offset };
+		}
+		if (size.Y == 2 && size.X > 1)
+		{
+			var path = new Dictionary<Vector2, Vector2>();
+			for (int x = 0; x < size.X - 1; x++)
 			{
-				Path = path;
-				return;
+				path[new(offset.X + x, offset.Y)] = new(offset.X + x + 1, offset.Y);
+			}
+			path[new(offset.X + size.X - 1, offset.Y)] = new(offset.X + size.X - 1, offset.Y + 1);
+			for (int x = size.X - 1; x > 0; x--)
+			{
+				path[new(offset.X + x, offset.Y + 1)] = new(offset.X + x - 1, offset.Y + 1);
+			}
+			path[new(offset.X, offset.Y + 1)] = new(offset.X, offset.Y);
+			return path;
+		}
+		if (size.X == 2 && size.Y > 1)
+		{
+			var path = new Dictionary<Vector2, Vector2>();
+			for (int y = 0; y < size.Y - 1; y++)
+			{
+				path[new(offset.X, offset.Y + y)] = new(offset.X, offset.Y + y + 1);
+			}
+			path[new(offset.X, offset.Y + size.Y - 1)] = new(offset.X + 1, offset.Y + size.Y - 1);
+			for (int y = size.Y - 1; y > 0; y--)
+			{
+				path[new(offset.X + 1, offset.Y + y)] = new(offset.X + 1, offset.Y + y - 1);
+			}
+			path[new(offset.X + 1, offset.Y)] = new(offset.X, offset.Y);
+			return path;
+		}
+		if (Int32.IsEvenInteger(size.Y) && size.X > 1)
+		{
+			var top = HamiltonCycle(offset, new(size.X, 2));
+			var rest = HamiltonCycle(new(offset.X, offset.Y + 2), new(size.X, size.Y - 2));
+
+			foreach (var (from, to) in top)
+			{
+				rest[from] = to;
 			}
 
-			foreach (var next in current.Neighbors)
-			{
-				if (!pathTo.ContainsKey(next) && IsOpen(next))
-				{
-					pathTo[next] = path.Add(next);
-					toVisit.Enqueue(next);
-				}
-			}
+			rest[new(offset.X, offset.Y + 2)] = new(offset.X, offset.Y + 1);
+			rest[new(offset.X + 1, offset.Y + 1)] = new(offset.X + 1, offset.Y + 2);
+
+			return rest;
 		}
+		if (Int32.IsEvenInteger(size.X) && size.Y > 1)
+		{
+			var top = HamiltonCycle(offset, new(2, size.Y));
+			var rest = HamiltonCycle(new(offset.X + 2, offset.Y), new(size.X - 2, size.Y));
+
+			foreach (var (from, to) in top)
+			{
+				rest[from] = to;
+			}
+
+			rest[new(offset.X + 2, offset.Y)] = new(offset.X + 1, offset.Y);
+			rest[new(offset.X + 1, offset.Y + 1)] = new(offset.X + 2, offset.Y + 1);
+
+			return rest;
+		}
+		throw new Exception("Not Hamiltonian");
 	}
 
 	public bool IsOpen(Vector2 pos)
